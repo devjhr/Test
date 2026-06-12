278e6a39f5d9bcd29d223f34e951b733da0d8837
jahangir <jahangir@gmail.com>
2026-06-12 21:50:41 +0600

    test3

--- BlameTest.java
+++ BlameTest.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Line 1");
         System.out.println("Modified Line 2");
-        System.out.println("Line 3");
+        System.out.println("Modified Line 3");
     }
 }
